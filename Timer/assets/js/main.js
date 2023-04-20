2eaab94a458ae30281f41034f5ee977bc5cae2d8
Guilherme Araújo Sena <127695147+guiaraujoJunior@users.noreply.github.com>
2023-04-20 11:32:27 -0300

    Add files via upload

---
+++ Timer/assets/js/main.js
@@ -0,0 +1,37 @@
+function getTimeFromSeconds(seconds) { 
+  const date = new Date(seconds * 1000);
+  return date.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
+console.log(getTimeFromSeconds(10));
+const clock = document.querySelector('.clock');
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const finish = document.querySelector('.zero');
+let seconds = 0;
+let timer;
+function startClock() {
+  timer = setInterval(function() {
+    seconds++;
+    clock.innerHTML = getTimeFromSeconds(seconds);
+  }, 1000)
+}
+document.addEventListener('click', function(e){
+  const el = e.target;
+  if(el.classList.contains('zero')) {
+  clearInterval(timer);
+  clock.innerHTML = '00:00:00';
+  seconds = 0;
+  }
+  if(el.classList.contains('pause')) {
+  clearInterval(timer);
+  clock.classList.add('paused');
+  }
+  if(el.classList.contains('start')) {
+  clock.classList.remove('paused');
+  clearInterval(timer);
+  startClock();
+  }
+})
